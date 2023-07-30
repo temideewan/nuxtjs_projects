ef7674d751af6939d9f5702cd831bb71b8036064
Omoyajowo Temidayo <temideewan@gmail.com>
2023-07-30 19:11:35 +0100

    chore: create initial folder structures and laysout

--- td_twitter_clone/components/Sidebar/Left.vue
+++ td_twitter_clone/components/Sidebar/Left.vue
@@ -1,10 +1,21 @@
 <template>
-  <div>
-    <div class="w-8 h-8">
-      <LogoTwitter />
+  <div class="h-screen flex flex-col">
+    <div class="p-2 my-2 rounded-full hover:bg-blue-50 w-min
+      dark:hover:bg-white/20
+    "
+    :class="defaultTransition"
+    >
+      <nuxt-link>
+        <div class="w-8 h-8">
+          <LogoTwitter />
+        </div>
+      </nuxt-link>
     </div>
   </div>
 </template>
-<script setup>
 
+
+<script setup>
+ const { defaultTransition} = useTailwindConfig();
 </script>
+
