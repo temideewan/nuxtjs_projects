743207834511750cc6079732745b4be5e06dc0df
Omoyajowo Temidayo <temideewan@gmail.com>
2023-08-02 21:24:49 +0100

    feat: left sidebar intro

---
+++ td_twitter_clone/components/Sidebar/Left/index.vue
@@ -0,0 +1,90 @@
+<template>
+  <div class="h-screen flex flex-col">
+    <div
+      class="p-2 my-2 rounded-full hover:bg-blue-50 w-min dark:hover:bg-white/20"
+      :class="defaultTransition"
+    >
+      <nuxt-link>
+        <div class="w-8 h-8">
+          <LogoTwitter />
+        </div>
+      </nuxt-link>
+    </div>
+    <div class="mt-2 space-y-3">
+      <SidebarLeftTab active>
+        <template v-slot:icon>
+          <HomeIcon />
+        </template>
+        <template v-slot:name> Home </template>
+      </SidebarLeftTab>
+
+      <SidebarLeftTab active>
+        <template v-slot:icon>
+          <HashtagIcon />
+        </template>
+        <template v-slot:name> Explore </template>
+      </SidebarLeftTab>
+
+      <SidebarLeftTab active>
+        <template v-slot:icon>
+          <BellIcon />
+        </template>
+        <template v-slot:name> Notifications </template>
+      </SidebarLeftTab>
+
+      <SidebarLeftTab active>
+        <template v-slot:icon>
+          <InboxIcon />
+        </template>
+        <template v-slot:name> Messages </template>
+      </SidebarLeftTab>
+
+      <SidebarLeftTab active>
+        <template v-slot:icon>
+          <BookmarkIcon/>
+        </template>
+        <template v-slot:name> Bookmarks </template>
+      </SidebarLeftTab>
+
+      <SidebarLeftTab active>
+        <template v-slot:icon>
+          <DocumentTextIcon />
+        </template>
+        <template v-slot:name> Lists </template>
+      </SidebarLeftTab>
+
+      <SidebarLeftTab active>
+        <template v-slot:icon>
+          <UserIcon />
+        </template>
+        <template v-slot:name> Profile </template>
+      </SidebarLeftTab>
+
+      <SidebarLeftTab active>
+        <template v-slot:icon>
+          <EllipsisHorizontalCircleIcon/>
+        </template>
+        <template v-slot:name> More </template>
+      </SidebarLeftTab>
+    </div>
+  </div>
+</template>
+
+
+<script setup>
+import {
+  HomeIcon,
+} from "@heroicons/vue/20/solid";
+import {
+  HashtagIcon,
+  BellIcon,
+  InboxIcon,
+  BookmarkIcon,
+  DocumentTextIcon,
+  UserIcon,
+  EllipsisHorizontalCircleIcon,
+} from "@heroicons/vue/24/outline";
+
+const { defaultTransition } = useTailwindConfig();
+</script>
+
